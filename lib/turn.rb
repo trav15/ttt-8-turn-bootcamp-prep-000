a8973f592ec8d6b16b93e41496bdfe4cb7096c73
Travis Tokuyama <travistokuyama@gmail.com>
2018-09-06 19:29:41 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,7 +6,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def valid_move(board, index)
+def valid_move?(board, index)
   if index.between?(0,8)
     if !position_taken?(board, index)
       true
